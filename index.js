e9c2549e5d41b83bb71eb55f2d69914656fc3aa9
bilalashraf1710 <fakhar.mehdi@ocloudsolutions.net>
2023-11-23 19:53:17 +0500

    solution implemented

---
+++ index.js
@@ -0,0 +1,17 @@
+async function delayedPromiseChain(promiseFunctions, delay) {
+    let answer;
+    for await (const x of promiseFunctions) {
+        answer = await x().then(data => data)
+        await setTimeout(() => { }, delay)
+    }
+    return new Promise(resolve => resolve(answer))
+}
+
+const promise1 = () => new Promise(resolve => setTimeout(() => resolve(1), 1000));
+const promise2 = () => new Promise(resolve => setTimeout(() => resolve(2), 500));
+const promise3 = () => new Promise(resolve => setTimeout(() => resolve(3), 800));
+const promise4 = () => new Promise(resolve => setTimeout(() => resolve(4), 300));
+const p = [promise1, promise2, promise3, promise4];
+const d = 1000;
+
+delayedPromiseChain(p, d).then(data => console.log("Promise(returned by the function), \nafter resolving....\nReturned: ", data))
