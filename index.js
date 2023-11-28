8ecf37765a23c28a0b7908a679bf2e4ded0c2453
bilalashraf1710 <fakhar.mehdi@ocloudsolutions.net>
2023-11-28 20:22:05 +0500

    names changed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-async function delayedPromiseChain(promiseFunctions, delay) {
+const delayedPromiseChain = async (promiseFunctions, delay) =>{
     let answer;
     for await (const x of promiseFunctions) {
         answer = await x().then(data => data)
@@ -11,7 +11,8 @@ const promise1 = () => new Promise(resolve => setTimeout(() => resolve(1), 1000)
 const promise2 = () => new Promise(resolve => setTimeout(() => resolve(2), 500));
 const promise3 = () => new Promise(resolve => setTimeout(() => resolve(3), 800));
 const promise4 = () => new Promise(resolve => setTimeout(() => resolve(4), 300));
-const p = [promise1, promise2, promise3, promise4];
-const d = 1000;
+const promises = [promise1, promise2, promise3, promise4];
+const delay = 1000;
 
-delayedPromiseChain(p, d).then(data => console.log("Promise(returned by the function), \nafter resolving....\nReturned: ", data))
+delayedPromiseChain(promises, delay).then(data => console.log("Promise(returned by the function), \nafter resolving....\nReturned: ", data))
+
